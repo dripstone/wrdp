aa7c249d842598d3ea12e8ce906926fed9740e22
dripstone <254940009@qq.com>
2015-10-27 15:43:12 +0800

    提交

--- wrdpframework/workflow/src/main/java/com/huiju/workflow/config/ActivitiConfig.java
+++ wrdpframework/workflow/src/main/java/com/huiju/workflow/config/ActivitiConfig.java
@@ -26,70 +26,76 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 @PropertySource(value = { "classpath:application.properties" })
 public class ActivitiConfig {
-	@Autowired
-	private Environment env;
-	@Autowired
-	private DataSource dataSource;
-	@Autowired
-	private PlatformTransactionManager transactionManager;
+    @Autowired
+    private Environment env;
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
 
-	// private String databaseSchemaUpdate = "DRIPSTONE";
-	private boolean jobExecutorActivate = false;
+    // private String databaseSchemaUpdate = "DRIPSTONE";
+    private boolean jobExecutorActivate = false;
 
-	public @Bean SpringProcessEngineConfiguration processEngineConfiguration() throws IOException {
-		SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
-		configuration.setDataSource(dataSource);
-		configuration.setTransactionManager(transactionManager);
-		// 数据库名oracle是用户名，注：参数必须大写
-		// configuration.setDatabaseSchema(databaseSchemaUpdate);
-		configuration.setJobExecutorActivate(jobExecutorActivate);
-		String dbCreate = env.getProperty("activiti.db_create");
-		if ("true".equals(dbCreate)) {
-			configuration.setDatabaseSchemaUpdate(env.getProperty("activiti.database_schema_Update"));
-			configuration.buildProcessEngine();
-		}
-		ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
-		Resource[] bpmnResources = resourcePatternResolver.getResources("classpath*:**/*.bpmn");
-		configuration.setDeploymentResources(bpmnResources);
-		// List<SessionFactory> sessionFactoryList = new
-		// ArrayList<SessionFactory>();
-		// sessionFactoryList.add(new SmartGapGroupEntityManagerFactory());
-		// sessionFactoryList.add(new SmartGapUserEntityManagerFactory());
-		// configuration.setCustomSessionFactories(sessionFactoryList);
-		// 集成到开发平台中时，需要设置false，不创建用户表，创建视图来代替这些用户，视图数据来源为平台中的用户数据
-		// configuration.setDbIdentityUsed(false);
+    public @Bean SpringProcessEngineConfiguration processEngineConfiguration()
+            throws IOException {
+        SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+        configuration.setDataSource(dataSource);
+        configuration.setTransactionManager(transactionManager);
+        // 数据库名oracle是用户名，注：参数必须大写
+        // configuration.setDatabaseSchema(databaseSchemaUpdate);
+        configuration.setJobExecutorActivate(jobExecutorActivate);
+        String dbCreate = env.getProperty("activiti.db_create");
+        if ("true".equals(dbCreate)) {
+            configuration.setDatabaseSchemaUpdate(
+                    env.getProperty("activiti.database_schema_Update"));
+            configuration.buildProcessEngine();
+        }
+        ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        Resource[] bpmnResources = resourcePatternResolver
+                .getResources("classpath*:**/*.bpmn");
+        configuration.setDeploymentResources(bpmnResources);
+        // List<SessionFactory> sessionFactoryList = new
+        // ArrayList<SessionFactory>();
+        // sessionFactoryList.add(new SmartGapGroupEntityManagerFactory());
+        // sessionFactoryList.add(new SmartGapUserEntityManagerFactory());
+        // configuration.setCustomSessionFactories(sessionFactoryList);
+        // 集成到开发平台中时，需要设置false，不创建用户表，创建视图来代替这些用户，视图数据来源为平台中的用户数据
+        // configuration.setDbIdentityUsed(false);
 
-		return configuration;
-	}
+        return configuration;
+    }
 
-	public @Bean ProcessEngineFactoryBean processEngine(SpringProcessEngineConfiguration processEngineConfiguration) {
-		ProcessEngineFactoryBean factory = new ProcessEngineFactoryBean();
-		factory.setProcessEngineConfiguration(processEngineConfiguration);
-		return factory;
-	}
+    public @Bean ProcessEngineFactoryBean processEngine(
+            SpringProcessEngineConfiguration processEngineConfiguration) {
+        ProcessEngineFactoryBean factory = new ProcessEngineFactoryBean();
+        factory.setProcessEngineConfiguration(processEngineConfiguration);
+        return factory;
+    }
 
-	public @Bean RepositoryService repositoryService(ProcessEngine processEngine) {
-		return processEngine.getRepositoryService();
-	}
+    public @Bean RepositoryService repositoryService(
+            ProcessEngine processEngine) {
+        return processEngine.getRepositoryService();
+    }
 
-	public @Bean RuntimeService runtimeService(ProcessEngine processEngine) {
-		return processEngine.getRuntimeService();
-	}
+    public @Bean RuntimeService runtimeService(ProcessEngine processEngine) {
+        return processEngine.getRuntimeService();
+    }
 
-	public @Bean TaskService taskService(ProcessEngine processEngine) {
-		TaskService taskService = processEngine.getTaskService();
-		return taskService;
-	}
+    public @Bean TaskService taskService(ProcessEngine processEngine) {
+        TaskService taskService = processEngine.getTaskService();
+        return taskService;
+    }
 
-	public @Bean HistoryService historyService(ProcessEngine processEngine) {
-		return processEngine.getHistoryService();
-	}
+    public @Bean HistoryService historyService(ProcessEngine processEngine) {
+        return processEngine.getHistoryService();
+    }
 
-	public @Bean ManagementService managementService(ProcessEngine processEngine) {
-		return processEngine.getManagementService();
-	}
+    public @Bean ManagementService managementService(
+            ProcessEngine processEngine) {
+        return processEngine.getManagementService();
+    }
 
-	public @Bean IdentityService IdentityService(ProcessEngine processEngine) {
-		return processEngine.getIdentityService();
-	}
+    public @Bean IdentityService IdentityService(ProcessEngine processEngine) {
+        return processEngine.getIdentityService();
+    }
 }
--- wrdpframework/workflow/src/main/java/com/huiju/workflow/controller/Test.java
+++ wrdpframework/workflow/src/main/java/com/huiju/workflow/controller/Test.java
@@ -19,40 +19,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test/")
 public class Test {
-	@Autowired
-	ProcessEngine processEngine;
+    @Autowired
+    ProcessEngine processEngine;
 
-	@RequestMapping
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws BeansException, Exception {
-		// 获取 RepositoryService
-		// RepositoryService repositoryService =
-		// processEngine.getRepositoryService();
-		RuntimeService runtimeService = processEngine.getRuntimeService();
-		// 使用 RepositoryService 部署流程定义
-		// repositoryService.createDeployment().addClasspathResource("bpmn/MyProcess.bpmn").deploy();
-		// 使用 RuntimeService 创建一个流程的实例
-		String procId = runtimeService.startProcessInstanceByKey("myProcess").getId();
+    @RequestMapping
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws BeansException, Exception {
+        // 获取 RepositoryService
+        // RepositoryService repositoryService =
+        // processEngine.getRepositoryService();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        // 使用 RepositoryService 部署流程定义
+        // repositoryService.createDeployment().addClasspathResource("bpmn/MyProcess.bpmn").deploy();
+        // 使用 RuntimeService 创建一个流程的实例
+        String procId = runtimeService.startProcessInstanceByKey("myProcess")
+                .getId();
 
-		// runtimeService.startProcessInstanceByKey("myProcess", "aaa");
+        // runtimeService.startProcessInstanceByKey("myProcess", "aaa");
 
-		// 获取 TaskService 获取任务服务
-		TaskService taskService = processEngine.getTaskService();
-		// 使用 TaskService 获取指定用户组的 Task 列表并使用指定用户领取这些任务
-		List<Task> tasks = taskService.createTaskQuery().taskCandidateUser("lisi").list();
-		for (Task task : tasks) {
-			System.out.println("Following task is available for accountancy group: " + task.getName());
-			// 任务拾取，拾取任务后，其他人看不到该任务
-			taskService.claim(task.getId(), "zhangsan");
-			// taskService.complete(taskId, variables);
-		}
+        // 获取 TaskService 获取任务服务
+        TaskService taskService = processEngine.getTaskService();
+        // 使用 TaskService 获取指定用户组的 Task 列表并使用指定用户领取这些任务
+        List<Task> tasks = taskService.createTaskQuery()
+                .taskCandidateUser("lisi").list();
+        for (Task task : tasks) {
+            System.out.println(
+                    "Following task is available for accountancy group: "
+                            + task.getName());
+            // 任务拾取，拾取任务后，其他人看不到该任务
+            taskService.claim(task.getId(), "zhangsan");
+            // taskService.complete(taskId, variables);
+        }
 
-		System.out
-				.println("Number of tasks for fozzie: " + taskService.createTaskQuery().taskAssignee("fozzie").count());
+        System.out.println("Number of tasks for fozzie: "
+                + taskService.createTaskQuery().taskAssignee("fozzie").count());
 
-		// 使用 HistoryService 来查询指定流程实例的状态
-		HistoryService historyService = processEngine.getHistoryService();
-		HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery()
-				.processInstanceId(procId).singleResult();
-		System.out.println("Process instance end time: " + historicProcessInstance.getEndTime());
-	}
+        // 使用 HistoryService 来查询指定流程实例的状态
+        HistoryService historyService = processEngine.getHistoryService();
+        HistoricProcessInstance historicProcessInstance = historyService
+                .createHistoricProcessInstanceQuery().processInstanceId(procId)
+                .singleResult();
+        System.out.println("Process instance end time: "
+                + historicProcessInstance.getEndTime());
+    }
 }
